8e0ff07c5b4a53f9a0fb3b8d9d92d791ef6d18b0
kongdej <kongdejs@gmail.com>
2022-04-04 14:22:36 +0700

    toc

---
+++ toc.py
@@ -0,0 +1,20 @@
+import pdfplumber
+
+filename = './BPRP1/BPRP1-2-2.pdf'
+
+with pdfplumber.open(filename) as pdf:
+    n = len(pdf.pages)
+    print (pdf.metadata)
+    print ('number of pages', n)
+    
+    for i in range(n):
+        text = pdf.pages[i].extract_text(x_tolerance=1)
+        x = text.find("TableofContent")
+        if x != -1:
+            print(text)
+
+        x = text.find("Table of Content")
+        if x != -1:
+            print(text)
+        
+        print(i)
